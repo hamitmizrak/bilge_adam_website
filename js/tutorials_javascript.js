2b1725b3f86d8db86c70c40c5b6a0fe8c5b06615
hamitmizrak <hamitmizrak@gmail.com>
2021-12-21 15:40:20 +0300

    javascript

---
+++ js/tutorials_javascript.js
@@ -0,0 +1,59 @@
+//js comment
+/* multiple comment*/
+// window.alert("Ekranda göster");
+// alert("kısaltılmış hali");
+// console.log("console log");
+// console.info("console info");
+// console.error("console error");
+
+// document.write("document write yazdım")
+
+//*************************************************************************
+//type
+
+// var sayi="kelime",sayi2;
+// sayi=true;
+// //sayi=34.56;
+// //sayi=44;
+
+// alert(typeof sayi)
+// alert(sayi);
+
+//***********************************************************************
+// var sayi2 = "44",
+//   result;
+// result = sayi2  / "22";
+// alert(result);
+
+//*************************************************************************
+//global değişkenler için kullaalım
+// var deneme1 = "485";
+// deneme1 = true;
+
+// //local dğişkenker
+// let deneme2 = "5555";
+// deneme2 = true;
+
+// //değişken değerinideğiştiremezsiniz
+// const deneme3 = "5555";
+// deneme3 = "99656";
+
+//*************************************************************************
+// var $44_deneme=55;
+// alert($44_deneme)
+
+//*************************************************************************
+// var sayi = 55;
+
+//js dizi
+// var dizi = ["asd", true, 55];
+// console.log(dizi);
+// alert(dizi[0]);
+
+//js object
+var object = {
+  adi: "Hamit",
+  soyadi: "Mızrak",
+  teknoloji: ["Java", "JSP"],
+};
+console.log(object);
